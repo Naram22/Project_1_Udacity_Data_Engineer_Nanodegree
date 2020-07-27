3eaaae1b14aede53c55d27f5067c427074f396b3
maran_many <my@personal>
2020-07-27 22:58:00 +0200

    add NOT NULL constraint and better dealing with conflicts on updating user_id

--- sql_queries.py
+++ sql_queries.py
@@ -8,8 +8,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (songplay_id IDENTITY(0,1) PRIMARY KEY, start_time bigint, user_id int, level varchar, 
-song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar)
+songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (songplay_id serial PRIMARY KEY, start_time bigint NOT NULL, user_id int NOT NULL, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar)
 """)
 
 user_table_create = (""" CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar)
@@ -30,8 +29,7 @@ songplay_table_insert = (""" INSERT INTO songplays (start_time, user_id, level, 
 """)
 
 user_table_insert = (""" INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) 
-ON CONFLICT (user_id) 
-DO NOTHING;
+ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = (""" INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
